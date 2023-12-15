63222485d1316ea11d2b2a61718575b6f232b7c5
Khaled Salah <102256602+khaledsa1ah@users.noreply.github.com>
2023-12-15 19:34:41 +0200

    Update PrioritySchedulingAlgorithm.java

--- src/main/java/org/example/PrioritySchedulingAlgorithm.java
+++ src/main/java/org/example/PrioritySchedulingAlgorithm.java
@@ -1,15 +1,22 @@
 package org.example;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class PrioritySchedulingAlgorithm {
+    static Map<Process, List<Interval>> intervals = new HashMap<>();
+    public double averageWaitingTime;
+    public double averageTurnAroundTime;
+
+    public PrioritySchedulingAlgorithm(List<Process> list, int i) {
+        simulatePriorityScheduling(list, i);
+    }
+
     private static void simulatePriorityScheduling(List<Process> processes, int contextSwitchingTime) {
         int currentTime = 0;
         int totalWaitingTime = 0;
         int totalTurnaroundTime = 0;
         int processedProcesses = 0;
-
+        final int AGE_THRESHOLD = 5;
         // Sort processes based on arrival time
         processes.sort(Comparator.comparingInt(p -> p.arrivalTime));
 
@@ -24,43 +31,51 @@ public class PrioritySchedulingAlgorithm {
                         currentProcess = process;
                     }
                 }
+
+                if (process != currentProcess) {
+                    process.waitingTime++;
+                    if (process.waitingTime >= AGE_THRESHOLD) {
+                        process.priority--;
+                        process.waitingTime = 0;
+                    }
+                }
             }
 
             if (currentProcess != null) {
-                // Update waiting time
+
                 currentProcess.waitingTime = currentTime - currentProcess.arrivalTime;
 
-                // Update turnaround time
                 currentProcess.turnaroundTime = currentProcess.waitingTime + currentProcess.burstTime;
 
-                // Update total waiting and turnaround time
                 totalWaitingTime += currentProcess.waitingTime;
                 totalTurnaroundTime += currentProcess.turnaroundTime;
 
-                // Update current time
                 currentTime += currentProcess.burstTime;
 
-                // Remove the processed process
                 processes.remove(currentProcess);
 
-                // Print history update
+                Interval interval = new Interval(currentTime - currentProcess.burstTime, currentTime);
+                List<Interval> processIntervals = intervals.computeIfAbsent(currentProcess, k -> new ArrayList<>());
+                processIntervals.add(interval);
+
                 System.out.println("Process " + currentProcess.name + " executed from " +
                         (currentTime - currentProcess.burstTime) + " to " + currentTime);
 
                 processedProcesses++;
             } else {
-                // No process arrived yet, increment time
                 currentTime++;
             }
         }
 
-        // Calculate averages
         double avgWaitingTime = processedProcesses > 0 ? (double) totalWaitingTime / processedProcesses : 0;
         double avgTurnaroundTime = processedProcesses > 0 ? (double) totalTurnaroundTime / processedProcesses : 0;
 
-        // Output results
         System.out.println("\nPriority Scheduling Results:");
         System.out.println("Average Waiting Time: " + avgWaitingTime);
         System.out.println("Average Turnaround Time: " + avgTurnaroundTime);
+    }
+
+    public Map<Process, List<Interval>> returnIntervals() {
+        return intervals;
     }
 }
